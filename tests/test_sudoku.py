0d2b2f79deac05b8ccbd75d6a71fbc86656c88a8
Jonar519 <jonar.martinez@campusucc.edu.co>
2025-03-25 23:29:58 +0000

    Subiendo trabajos

--- tests/test_sudoku.py
+++ tests/test_sudoku.py
@@ -1,5 +1,24 @@
 import pytest
-from src.sudoku import *  # Importa el módulo desde el paquete src
+from src.sudoku import es_sudoku_valido
 
-def test_placeholder():
-    assert True  # TODO: Reemplazar con pruebas reales
+def test_sudoku_valido():
+    tablero_valido = [
+        [5, 3, 4, 6, 7, 8, 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        [1, 9, 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9, 7, 6, 1, 4, 2, 3],
+        [4, 2, 6, 8, 5, 3, 7, 9, 1],
+        [7, 1, 3, 9, 2, 4, 8, 5, 6],
+        [9, 6, 1, 5, 3, 7, 2, 8, 4],
+        [2, 8, 7, 4, 1, 9, 6, 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9]
+    ]
+    assert es_sudoku_valido(tablero_valido) is True
+
+def test_sudoku_invalido_fila():
+    tablero_invalido = [
+        [5, 5, 5, 6, 7, 8, 9, 1, 2],  # Fila con números repetidos
+        [6, 7, 2, 1, 9, 5, 3, 4, 8],
+        # ... resto del tablero ...
+    ]
+    assert es_sudoku_valido(tablero_invalido) is False
